4d0dd2da692b7e787cb4ecb174e694923bc04f07
Ivan Kasaija <kasaijaivan@gmail.com>
2018-07-01 13:07:58 -0400

    some changes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,5 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require almond
 //= require rails-ujs
 //= require_tree .
